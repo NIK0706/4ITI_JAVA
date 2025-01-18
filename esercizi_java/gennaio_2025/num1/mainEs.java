27c83e98527955210e287af4b654db6eed6bd0ca
NIK_ <nicolas.marciello07@gmail.com>
2025-01-18 03:22:58 +0100

    Modifica esercizio

--- esercizi_java/gennaio_2025/num1/mainEs.java
+++ esercizi_java/gennaio_2025/num1/mainEs.java
@@ -2,6 +2,9 @@ package esercizi_java.gennaio_2025.num1;
 
 public class mainEs {
 	public static void main(String[] args) {
+		potenza Potenza1 = new potenza(2);
 
+		System.out.println("pow:" + Potenza1.pow(2));
+		System.out.println("powRecursivo:" + Potenza1.powRecursive(2));
 	}
 }
